599e6cae355648333e5b497daa0abe05c9e768c5
Savio Fagundes <akirasfm@gmail.com>
2024-08-27 21:45:22 -0300

    Adicionar arquivos de projeto.

---
+++ TabelaUrnaEletronica/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TabelaUrnaEletronica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path:");
+            string path = Console.ReadLine();
+            try
+            {
+                Dictionary<string, int> dict = new Dictionary<string, int>();
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if(dict.ContainsKey(candidate))
+                        {
+                            dict[candidate] += votes;
+                        }
+                        else
+                        {
+                            dict[candidate] = votes;
+                        }
+                    }
+                    foreach(var item in dict)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
